32b7ab4d61184e65c0ae4c69159ddf6f58ee0f81
SHARP <ssharp9999@gmail.com>
2021-10-14 11:44:12 -0700

    event model start

---
+++ models/Event.js
@@ -0,0 +1,51 @@
+
+// color (could be a future)
+
+const { model, Schema } = require('mongoose')
+
+const Event = new Schema({
+ // event title
+ title: {
+  type: String,
+  trim: true,
+  required: [true, 'Please provide a title']
+ },
+ // event description
+ description: {
+  type: String,
+  trim: true,
+  required: [true, 'Please provide a description']
+ },
+ // event date
+ date: {
+  type: Date,
+  required: [true, 'Please provide a date']
+ },
+ // start time
+ start: {
+  type: Date,
+  required: [true, 'Please provide a start time']
+ },
+ // end time
+ end: {
+  type: Date,
+  required: [true, 'Please provide an end time']
+ },
+ // event set for all day - optional
+ allDay: {
+  type: Boolean,
+ },
+ // event location - optional
+ location: {
+  type: String,
+  trim: true,
+ },
+ // reference user model
+ user: {
+  type: Schema.Types.ObjectId,
+  ref: 'User'
+ }
+}, { timestamps: true })
+
+// export model Event
+module.exports = model('Event', Event)
