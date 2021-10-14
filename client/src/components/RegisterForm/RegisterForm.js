87edfbdb372161a038de493ed15efb0247c47012
SHARP <ssharp9999@gmail.com>
2021-10-14 14:59:51 -0700

    register form template and functionality

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,74 +1,136 @@
 import { useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import UserAPI from '../../utils/UserAPI'
 
-const RegisterForm = () => {
- const [userState, setUserState] = useState({
-  name: '',
-  email: '',
-  username: '',
-  password: ''
- })
-
- const handleInputChange = ({ target: { name, value } }) => setUserState({ ...userState, [name]: value })
-
- const handleRegisterUser = event => {
-  event.preventDefault()
-  UserAPI.register(userState)
-   .then(() => {
-    alert('User Registered!')
-    setUserState({ ...userState, name: '', email: '', username: '', password: '' })
-   })
-   .catch(err => console.error(err))
- }
-
- return (
-  <Form>
-   <Form.Group className="mb-3" controlId="name">
-    <Form.Label>Name</Form.Label>
-    <Form.Control
-     type="text"
-     placeholder="Enter your name"
-     name="name"
-     value={userState.name}
-     onChange={handleInputChange} />
-   </Form.Group>
-   <Form.Group className="mb-3" controlId="email">
-    <Form.Label>Email</Form.Label>
-    <Form.Control
-     type="email"
-     placeholder="Enter your email"
-     name="email"
-     value={userState.email}
-     onChange={handleInputChange} />
-   </Form.Group>
-   <Form.Group className="mb-3" controlId="username">
-    <Form.Label>Username</Form.Label>
-    <Form.Control
-     type="text"
-     placeholder="Enter your username"
-     name="username"
-     value={userState.username}
-     onChange={handleInputChange} />
-   </Form.Group>
-   <Form.Group className="mb-3" controlId="password">
-    <Form.Label>Password</Form.Label>
-    <Form.Control
-     type="password"
-     placeholder="Enter your password"
-     name="password"
-     value={userState.password}
-     onChange={handleInputChange} />
-   </Form.Group>
-   <Button
-    variant="primary"
-    type="submit"
-    onClick={handleRegisterUser} >
-    Register
-   </Button>
-  </Form>
- )
+function RegisterFooter(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      <Link color="inherit" href="#">
+        Synergize
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default RegisterForm
+const theme = createTheme();
+
+export default function RegisterForm() {
+
+  const [userState, setUserState] = useState({
+    name: '',
+    username: '',
+    password: ''
+  })
+
+  const handleInputChange = ({ target: { name, value } }) => setUserState({ ...userState, [name]: value })
+
+  const handleRegisterUser = event => {
+    event.preventDefault()
+    UserAPI.register(userState)
+      .then(() => {
+        alert('User Registered! Sign In Now')
+        setUserState({ ...userState, name: '', username: '', password: '' })
+        window.location = '/signIn'
+      })
+      .catch(err => console.error(err))
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleRegisterUser} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="name"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  autoFocus
+                  value={userState.name}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  value={userState.username}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  value={userState.password}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 3 }}
+            >
+              Register
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link href="/signIn" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <RegisterFooter sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+// export default RegisterForm
