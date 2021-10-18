e2fccce6f3e2a8aca8776fa7219f7afa13c40b48
ssterling27 <samuel.steinberg27@gmail.com>
2021-10-18 13:01:54 -0700

    navbar changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, Component } from 'react'
 
 function Home() {
   return (
-    <div style={{ backgroundColor: 'blue', height: '100vh', width: '92vw', position: 'relative', float: 'right'}}>
+    <div id={'home'} style={{ backgroundColor: 'blue', height: '100vh', width: '92vw', position: 'relative', float: 'right'}}>
     </div>
   )
 }
