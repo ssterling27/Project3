b5577680181897140949eaccf29d49deae6acc7f
SHARP <ssharp9999@gmail.com>
2021-10-18 13:30:50 -0700

    merge changes from main

--- client/src/utils/UserAPI/UserAPI.js
+++ client/src/utils/UserAPI/UserAPI.js
@@ -1,19 +1,17 @@
 import axios from 'axios'
 
 const UserAPI = {
- getUser: () => axios.get('/api/user', {
+ getUser: _ => axios.get('/api/user', {
   headers: {
    Authorization: `Bearer ${localStorage.getItem('token')}`
   }
  }),
  register: user => axios.post('/api/users/register', user),
  login: user => axios.post('/api/users/login', user),
-
-  addFriend: (user_id, user) => axios.post(`/api/users/addFriend/${user_id} `, {
-    headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`
-  }
-})
-searchFriend: query => axios.post('/api/users/search', query)
+ addFriend: (user_id, user) => axios.post(`/users/addFriend/${user_id}`, {
+  headers: {
+   Authorization: `Bearer ${localStorage.getItem('token')}` }}, user),
+ searchFriend: query => axios.post('/api/users/search', query)
+}
 
 export default UserAPI
