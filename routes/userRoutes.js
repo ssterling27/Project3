871a9c3e423f4ff5e8df2eeb99ff4ba65d3148ab
SHARP <ssharp9999@gmail.com>
2021-10-21 10:00:21 -0700

    user route reg and login for error handle

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,13 +13,44 @@ router.get('/users', (req, res) => {
   .catch(err => console.log(err))
 })
 
-// register a new user
+// register a new user (username to lowercase to check for duplicates and place in db)
 router.post('/users/register', (req, res) => {
- const { name, username } = req.body
- User.register(new User({ name, username }), req.body.password, err => {
-  if (err) { console.log(err) }
-  res.sendStatus(200)
- })
+ // set the req.body and username to be lowercase to prevent duplicate usernames
+ const { name, username, password } = req.body
+ const usernameLowerCase = req.body.username.toLowerCase()
+
+ // variable to set registration status messages to check for blanks
+ let registrationStatus = { nameMsg: '', usernameMsg: '', passwordMsg: '' }
+
+ // if the name, username, or password length is less than one (blank) then set the registration status with the messages
+ name.length < 1 && (registrationStatus.nameMsg = 'Name cannot be blank')
+ username.length < 1 && (registrationStatus.usernameMsg = 'Username cannot be blank')
+ password.length < 1 && (registrationStatus.passwordMsg = 'Password cannot be blank')
+
+ // find all registered users and push the usernames into the registeredusernames array
+ let registeredUserNames = []
+ User.find({})
+  .then(users => {
+    users.forEach(user => {
+      registeredUserNames.push(user.username)
+    })
+  })
+  .catch(err => console.log(err))
+
+ // if there is a registration status (if blank from above) then unable to register
+ if (registrationStatus.nameMsg || registrationStatus.usernameMsg || registrationStatus.passwordMsg) {
+  res.json({ status: registrationStatus, regMessage: 'Unsuccessful. Input blank' })
+ } 
+ // else, register the new user (name, lowercase username, and password). If error, the lowercase username exists and not register. Else register user
+ else { User.register(new User({ name, username: usernameLowerCase }), req.body.password, err => {
+    if (err) { 
+      registeredUserNames.indexOf(username) !== -1 && (registrationStatus.username = 'Username already exists')
+      res.json({ status: registrationStatus, message: 'Registration unsuccessful. Username already exists.', err})
+      return
+    }
+    else { res.sendStatus(200) }
+   })
+ }
 })
 
 // login a user
@@ -29,6 +60,17 @@ router.post('/users/login', (req, res) => {
   res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
  })
 })
+
+//  ---------- USE THIS FOR THE PROD instead of user login route above (change all values to lowercase to prevent duplicates as done during registration) -----------
+
+// router.post('/users/login', (req, res) => {
+//   User.authenticate()(req.body.username.toLowerCase(), req.body.password, (err, user) => {
+//     if (err) { console.log(err) }
+//     res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
+//   })
+// })
+
+//  ---------- USE ABOVE FOR PROD -----------
 
 
 
