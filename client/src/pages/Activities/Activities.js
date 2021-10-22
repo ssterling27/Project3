40bf849c1a7135c2140a1713b6399f811638153c
SHARP <ssharp9999@gmail.com>
2021-10-22 14:21:16 -0700

    activities start

--- client/src/pages/Activities/Activities.js
+++ client/src/pages/Activities/Activities.js
@@ -1,7 +1,35 @@
+import { Button, Paper, Box, Typography, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia } from '@mui/material'
+
 function Activities() {
   return (
     <div id={'activities'} style={{ backgroundColor: 'yellow', height: '100vh', width: '92vw', position: 'relative', float: 'right' }}>
+      <Paper elevation={8} style={{ display: 'flex', justifyContent: 'center', marginBottom: '2%', margin: '2vw' }}>
+        <h1>Activities</h1>
+      </Paper>
+      <Grid item xs={12} sm={8} md={6}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height=""
+            image=""
+            alt=""
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Title
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum amet facilis excepturi, neque nesciunt aliquam assumenda nemo ea soluta quae praesentium et quos optio laboriosam, magnam unde cum cupiditate molestias.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
     </div>
+
   )
 }
 
