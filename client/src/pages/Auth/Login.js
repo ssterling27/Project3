588be32b1e1660e7b4001803b38101e343394540
SHARP <ssharp9999@gmail.com>
2021-10-22 17:52:06 -0700

    style for page background started

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,8 +1,11 @@
 import LogInForm from "../../components/SignInForm/SignInForm"
+import './Login.css'
 
 const Login = () => {
  return (
-  <LogInForm />
+  <div id={'loginPage'} style={{ height: '100vh', width: '100vw', position: 'relative', float: 'right' }}>
+   <LogInForm/>
+  </div>
  )
 }
 
